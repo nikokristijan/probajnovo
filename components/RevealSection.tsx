9eb8f093f67219fe722b19231e17d38f7333dd05
nikokristijan <niko.kristijan1@gmail.com>
2026-09-13 12:31:21 +0200

    Fix RevealSection: threshold 0 instead of 0.15
    Sekcije vece od ~85% visine viewporta (npr. "grand" galerija koja je jedan dugi vertikalni niz panoramskih fotografija, cesto 5000-9000px visoka) nikad nisu mogle dosegnuti prag od 15% vidljivosti pa su ostajale zauvijek nevidljive (opacity: 0). Prag spusten na 0 + rootMargin i dalje odgadja okidanje dok element ne udje ~40px u viewport.

--- components/RevealSection.tsx
+++ components/RevealSection.tsx
@@ -33,7 +33,7 @@ export default function RevealSection({
           obs.disconnect();
         }
       },
-      { threshold: 0.15, rootMargin: "0px 0px -40px 0px" }
+      { threshold: 0, rootMargin: "0px 0px -40px 0px" }
     );
     obs.observe(el);
     return () => obs.disconnect();
